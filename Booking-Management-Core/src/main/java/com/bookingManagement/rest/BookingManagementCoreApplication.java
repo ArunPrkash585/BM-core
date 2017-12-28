07dad4effbb3ce6fccff3402ebae3c982fab9559
arun <arun@Arun>
2017-12-29 00:06:07 +0530

    initial commit

---
+++ Booking-Management-Core/src/main/java/com/bookingManagement/rest/BookingManagementCoreApplication.java
@@ -0,0 +1,12 @@
+package com.bookingManagement.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingManagementCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingManagementCoreApplication.class, args);
+	}
+}
---
+++ Booking-Management-Core/src/test/java/com/bookingManagement/rest/BookingManagementCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bookingManagement.rest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookingManagementCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
